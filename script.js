f407d8ce510921bd1ed63f92cc70bd4a0d3f3145
SebastianS02 <stecsebastian2002@gmail.com>
2022-01-07 17:25:17 -0500

    delete-row

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let cells = 1;
+let cells = 3;
 let selectedColor = "red";
 let coloring = false;
 
-function addRows() {
+function addRows(){
   let table = document.getElementById("table");
   let row = document.createElement("tr");
   row.classList.add("row");
   table.appendChild(row);
-  for (let i = 0; i < cells; i++) {
+  for (let i = 0; i < cells; i++){
     let cell = document.createElement("td");
     helperFunction(cell);
     row.appendChild(cell);
@@ -18,9 +18,21 @@ function addColumns(){
     cells++;
     let newRows = document.getElementsByClassName("row");
     tr = [...newRows];
-    for (let i = 0; i < tr.length; i++) {
+    for (let i = 0; i < tr.length; i++){
       let cell = document.createElement("td");
       helperFunction(cell);
       tr[i].appendChild(cell);
     }
-  }
+}
+
+function removeRow(){
+    let rows2 = document.getElementsByClassName("row");
+    tr = [...rows2];
+    if (tr.length == 0){
+      return;
+    }
+    tr[tr.length - 1].parentNode.removeChild(tr[tr.length - 1]);
+    if (tr.length == 1){
+      cells = 1;
+    }
+}
